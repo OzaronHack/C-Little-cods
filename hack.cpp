c067e14e0090ab5248f39123a974ce1059610e12
OzaronHack <78981138+OzaronHack@users.noreply.github.com>
2021-02-12 23:33:56 +0400

    Create hack.cpp

---
+++ hack.cpp
@@ -0,0 +1,5 @@
+int[] maib(string[] *****arguments)_>{
+  DWORD SIGNATURA = bSendPacket()->FindSegnatura( "Ozaron#5101"/*Все нормално я здесь свой*/ );
+  bSendPacket(SIGNATURA)->HACK(bSendPacket(ESP)):::STD::ESP::WALLHACK:PAINT:AIMBO::::BATONET->HACK();
+  return (/*ozaron#5101*/)!~_
+}
